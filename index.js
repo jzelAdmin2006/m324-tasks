b9e242c39220ca4d7c147cbb9bb63db7d36cd0cc
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-11-28 10:12:02 +0100

    solve task JS-programming with libraries

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import {generate} from "random-words";
+import chalk from "chalk";
+
 console.log("Hello, World!");
+
+const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 });
+const word2 = generate();
+console.log(
+    `The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`
+);
