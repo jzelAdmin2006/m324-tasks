4b0fc13bf4b9b9f6645f0f9e46f627fada3b63a5
Johannes Zeller <jo.zeller@protonmail.com>
2024-11-29 22:37:56 +0100

    increase linter strictness

--- adaption/m324-tasks-adaption/src/main/java/com/jzel/Fibonacci.java
+++ adaption/m324-tasks-adaption/src/main/java/com/jzel/Fibonacci.java
@@ -1,15 +1,26 @@
 package com.jzel;
 
+/**
+ * This is a Calculator for the Fibonacci sequence.
+ */
 public class Fibonacci {
 
+    /**
+     * Calculate the fibonacci sequence.
+     *
+     * @param n number of sequence entries to calculate
+     * @return result according to nth number of sequence
+     */
     public int calculate(final int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Input must be non-negative");
         }
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+        if (n == 0) {
+            return 0;
+        }
 
-        int prev = 0, current = 1;
+        int prev = 0;
+        int current = 1;
 
         for (int i = 2; i <= n; i++) {
             final int next = prev + current;
--- adaption/m324-tasks-adaption/src/main/java/com/jzel/Main.java
+++ adaption/m324-tasks-adaption/src/main/java/com/jzel/Main.java
@@ -1,7 +1,15 @@
 package com.jzel;
 
+/**
+ * This is the main class for the demo.
+ */
 public class Main {
 
+    /**
+     * This is a demo.
+     *
+     * @param args Program args (not required in this case)
+     */
     public static void main(String[] args) {
         System.out.println("Fibonacci: " + new Fibonacci().calculate(10));
     }
